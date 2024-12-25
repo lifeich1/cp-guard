e79f81fac863b9544057eb7fbe25803b5b9a46d1
lifeich1 <lifeich0@gmail.com>
2024-12-26 01:43:31 +0800

    :bug: lint ParseResult

--- cp-guard/src/lib.rs
+++ cp-guard/src/lib.rs
@@ -3,60 +3,60 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct ParseResult {
-    name: String,
-    group: String,
-    url: String,
-    interactive: Option<bool>,
-    memory_limit: i64,
-    time_limit: i64,
-    tests: Vec<Testcase>,
-    test_type: String,
-    input: InputDesc,
-    output: OutputDesc,
-    languages: Option<LangSettings>,
-    batch: BatchDesc,
+    pub name: String,
+    pub group: String,
+    pub url: String,
+    pub interactive: Option<bool>,
+    pub memory_limit: i64,
+    pub time_limit: i64,
+    pub tests: Vec<Testcase>,
+    pub test_type: String,
+    pub input: InputDesc,
+    pub output: OutputDesc,
+    pub languages: Option<LangSettings>,
+    pub batch: BatchDesc,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct Testcase {
-    input: String,
-    output: String,
+    pub input: String,
+    pub output: String,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct InputDesc {
     #[serde(rename = "type")]
-    kind: String,
-    file_name: Option<String>,
-    pattern: Option<String>,
+    pub kind: String,
+    pub file_name: Option<String>,
+    pub pattern: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct OutputDesc {
     #[serde(rename = "type")]
-    kind: String,
-    file_name: Option<String>,
+    pub kind: String,
+    pub file_name: Option<String>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct LangSettings {
-    java: Option<JavaLangSetting>,
+    pub java: Option<JavaLangSetting>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct JavaLangSetting {
-    main_class: String,
-    task_class: String,
+    pub main_class: String,
+    pub task_class: String,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct BatchDesc {
-    id: String,
-    size: u64,
+    pub id: String,
+    pub size: u64,
 }
