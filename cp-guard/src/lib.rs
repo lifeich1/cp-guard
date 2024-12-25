b589cd04e037ea44510a404251afda1826e3d74d
lifeich1 <lifeich0@gmail.com>
2024-12-26 01:39:59 +0800

    :construction: deser parse result

--- cp-guard/src/lib.rs
+++ cp-guard/src/lib.rs
@@ -1,1 +1,62 @@
+use serde::Deserialize;
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ParseResult {
+    name: String,
+    group: String,
+    url: String,
+    interactive: Option<bool>,
+    memory_limit: i64,
+    time_limit: i64,
+    tests: Vec<Testcase>,
+    test_type: String,
+    input: InputDesc,
+    output: OutputDesc,
+    languages: Option<LangSettings>,
+    batch: BatchDesc,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Testcase {
+    input: String,
+    output: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct InputDesc {
+    #[serde(rename = "type")]
+    kind: String,
+    file_name: Option<String>,
+    pattern: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct OutputDesc {
+    #[serde(rename = "type")]
+    kind: String,
+    file_name: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LangSettings {
+    java: Option<JavaLangSetting>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct JavaLangSetting {
+    main_class: String,
+    task_class: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchDesc {
+    id: String,
+    size: u64,
+}
--- cp-guard/src/main.rs
+++ cp-guard/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::post, Json, Router};
+use cp_guard::ParseResult;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new().route("/", post(handle_parse_result));
+
+    // use impossible acmX port.
+    let listener = tokio::net::TcpListener::bind("localhost:10042")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handle_parse_result(Json(payload): Json<ParseResult>) -> (StatusCode, &'static str) {
+    println!("recv parse result: {:?}", payload);
+    (StatusCode::CREATED, "Gotta")
 }
