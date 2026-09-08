d5b5e0f447c4c6b27c0c946d7060ac3edbe6098a
lifeich1 <lifeich0@gmail.com>
2026-09-08 13:32:12 +0800

    📦 flake: update nixpkgs input and add derivation metadata
    - Bump nixpkgs to 26.11pre and flake-utils to latest
    - Add meta.mainProgram and meta.description to the package
    - Reorganize devShell buildInputs structure

--- flake.nix
+++ flake.nix
@@ -30,27 +30,32 @@
             rm -f $out/bin/xtask # devops, strip from release
           '';
 
+          meta = {
+            mainProgram = "cp-guard";
+            description = "cp-guard 是一个 Rust 实现的本地监听服务（守护进程），用于自动保存竞技编程（Competitive Programming）比赛的题目数据";
+          };
+
           # For other makeRustPlatform features see:
           # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
         };
 
-        # Executed by `nix run`
-        apps.default = utils.lib.mkApp { drv = packages.default; };
-
         # Used by `nix develop`
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (with toolchain; [
+          buildInputs =
+            with pkgs;
+            [
+
+              clippy
+              rustfmt
+              pkg-config
+              cargo-edit
+              nix-update
+            ]
+            ++ (with toolchain; [
               cargo
               rustc
               rustLibSrc
-            ])
-            clippy
-            rustfmt
-            pkg-config
-            cargo-edit
-            nix-update
-          ];
+            ]);
 
           # Specify the rust-src path (many editors rely on this)
           RUST_SRC_PATH = "${toolchain.rustLibSrc}";
